4484174284368462e377bec2062cbc14c6bc09c9
Mauro Mereu <mauromereu@gmail.com>
2023-04-13 16:47:10 +0200

    First commit

---
+++ src/preload.js
@@ -0,0 +1,10 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+window.addEventListener('DOMContentLoaded',()=>{
+    const replaceText  = (selector, text) => {
+        const element = document.getElementById(selector);
+        if(element) element.innerText = text;
+    }
+    replaceText('node-version',process.versions['node']);
+})
